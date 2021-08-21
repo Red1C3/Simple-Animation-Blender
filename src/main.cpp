9e1f02901755cff6e3c2b5b7284ac08a9c26b925
Mohammad Issawi <2000issawi@gmail.com>
2021-08-21 22:27:19 +0300

    Added License

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,24 @@
+/*MIT License
+
+Copyright (c) 2021 Mohammad Issawi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.*/
 #include <simple-animation-blender/Application.h>
 #include <simple-animation-blender/GUI.h>
 #include <simple-animation-blender/Animator.h>
@@ -59,6 +80,11 @@ int main(int argc, char **argv)
                 ImGui::SliderFloat("Blending Factor", &blendingFactorHolder, 0.0f, 1.0f, "%.3f", 1.0f);
             ImGui::ColorEdit3("Mesh Color", colorHolder);
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::Separator();
+            ImGui::Text("+/- for zooming");
+            ImGui::Separator();
+            ImGui::Text("Achieved with Slowly Walking Kittens");
+            ImGui::Text("Simple Animation Blender by Mohammad Issawi a.k.a RedDeadAlice");
             ImGui::End();
             ImGui::Render();
             ImDrawData *imGuiDrawData = ImGui::GetDrawData();
